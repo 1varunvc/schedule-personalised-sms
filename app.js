4a2611376994f1995b9c9a0490bd1db69f1f145a
Varun Chawla <1varunvc@gmail.com>
2024-03-27 14:11:59 +0530

    Remove the random delay.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ However, to test sendSMS.js directly from the terminal locally, use the command 
 
 For testing, you can also use:
 netlify dev (in one terminal)
-curl -X POST http://localhost:8888/.netlify/functions/sendSMS -H "Content-Type: application/json" (in another terminal.)
+curl -X GET http://localhost:8888/.netlify/functions/sendSMS -H "Content-Type: application/json" (in another terminal.)
 
 Modify the port 8888 if your netlify dev server uses a different one.
 */
@@ -12,39 +12,29 @@ require('dotenv').config();
 const messages = require('./messages');
 const logger = require('./logger');
 
-// Function to simulate a random delay
-function randomDelay(minSeconds, maxSeconds) {
-    return Math.floor(Math.random() * (maxSeconds - minSeconds + 1) + minSeconds);
-}
-
 // The main function to run the script
 (async () => {
-    const delaySeconds = randomDelay(0, 180);
     const randomIndex = Math.floor(Math.random() * messages.length);
     const messageToSend = messages[randomIndex];
 
+    logger.info('SMS scheduler started.');
     logger.info(`Sending message at the index: ${randomIndex}`);
-    logger.info(`Sending message: "${messageToSend}" to ${process.env.PHONE_NUMBER_1} with a delay of ` + (delaySeconds/60).toFixed(2) + ` minutes.`);
+    logger.info(`Sending message: "${messageToSend}" to PHONE_NUMBER_1.`);
 
-    setTimeout(async () => {
-        fetch("https://sms-scheduler-01.netlify.app/.netlify/functions/sendSMS", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({phoneNumber: process.env.PHONE_NUMBER_1, message: messageToSend})
+    fetch("https://sms-scheduler-01.netlify.app/.netlify/functions/sendSMS", {
+        method: "GET"
+        // headers: {"Content-Type": "application/json"},
+        // body: JSON.stringify({phoneNumber: process.env.PHONE_NUMBER_1, message: messageToSend})
+    })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                logger.info("Message sent successfully.");
+            } else {
+                logger.info("Unable to send the scheduled message after retries.");
+            }
         })
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    logger.info("Message sent successfully.");
-                } else {
-                    logger.info("Unable to send the scheduled message after retries.");
-                }
-            })
-            .catch(error => {
-                logger.error("Request failed: " + error);
-            });
-    }, delaySeconds * 1000); // Convert seconds to milliseconds for setTimeout
+        .catch(error => {
+            logger.error("Request failed: " + error);
+        });
 })();
-
-
-logger.info('SMS scheduler with approximate time started.');
--- netlify/functions/sendSMS.js
+++ netlify/functions/sendSMS.js
@@ -1,15 +1,10 @@
 const logger = require('../../logger.js')
 const messages = require('../../messages');
 
-function randomDelay(minSeconds, maxSeconds) {
-    return Math.floor(Math.random() * (maxSeconds - minSeconds + 1) + minSeconds);
-}
-
 exports.handler = async (event) => {
     const twilio = require('twilio');
     const validator = require('validator');
     const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-    const delaySeconds = randomDelay(0, 180);
     const randomIndex = Math.floor(Math.random() * messages.length);
     const messageToSend = messages[randomIndex];
     const phoneNumber = process.env.PHONE_NUMBER_2;
@@ -18,7 +13,7 @@ exports.handler = async (event) => {
     // // Extract phoneNumber and message from event body
     // const {phoneNumber, message} = JSON.parse(event.body);
 
-    logger.info('SMS scheduler (with approximate time) started.');
+    logger.info('SMS scheduler started.');
 
     // Validate phone number
     if (!validator.isMobilePhone(phoneNumber, 'any', {strictMode: false})) {
@@ -33,9 +28,8 @@ exports.handler = async (event) => {
 
         try {
             logger.info(`Sending message at the index: ${randomIndex}`);
-            logger.info(`Sending message: "${message}" to PHONE_NUMBER_2 with a delay of ` + (delaySeconds/60).toFixed(2) + ` minutes.`);
+            logger.info(`Sending message: "${message}" to PHONE_NUMBER_2.`);
 
-            await new Promise(resolve => setTimeout(resolve, delaySeconds * 1000)); // Convert seconds to milliseconds for setTimeout
             const response = await client.messages.create({
                 body: message,
                 from: process.env.TWILIO_PHONE_NUMBER,
