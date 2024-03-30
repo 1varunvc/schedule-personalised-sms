9951a7e97646adec9a1fc1c15a0be2341b7b96fd
Varun Chawla <1varunvc@gmail.com>
2024-03-30 15:48:11 +0530

    Remove app.js as it's not required for Netlify. Add phone number masking for logs.

--- netlify/functions/sendSMS.js
+++ netlify/functions/sendSMS.js
@@ -1,6 +1,22 @@
 const logger = require('../../logger.js')
 const messages = require('../../messages');
 
+function maskPhoneNumber(phoneNumber) {
+    // Check if the phoneNumber is valid
+    if (!phoneNumber || phoneNumber.length < 4) {
+        return phoneNumber; // Return the original string if it's too short to mask
+    }
+
+    // Extract the last 4 digits
+    const lastFourDigits = phoneNumber.slice(-4);
+
+    // Create the masked part by repeating 'X' for the rest of the length
+    const maskedSection = 'X'.repeat(phoneNumber.length - 4);
+
+    // Combine the masked part with the last 4 digits
+    return maskedSection + lastFourDigits;
+}
+
 exports.handler = async (event) => {
     const twilio = require('twilio');
     const validator = require('validator');
@@ -8,6 +24,7 @@ exports.handler = async (event) => {
     const randomIndex = Math.floor(Math.random() * messages.length);
     const messageToSend = messages[randomIndex];
     const phoneNumber = process.env.PHONE_NUMBER_2;
+    const maskedPhoneNumber = maskPhoneNumber(phoneNumber);
 
     // The following is commented out as we are no longer using the body of a request to assign the phoneNumber and the message.
     // // Extract phoneNumber and message from event body
@@ -17,7 +34,7 @@ exports.handler = async (event) => {
 
     // Validate phone number
     if (!validator.isMobilePhone(phoneNumber, 'any', {strictMode: false})) {
-        logger.error(`Invalid phone number: PHONE_NUMBER_2`);
+        logger.error(`Invalid phone number: ${maskedPhoneNumber}`);
         return {statusCode: 400, body: JSON.stringify({success: false, message: "Invalid phone number."})};
     }
 
@@ -28,17 +45,17 @@ exports.handler = async (event) => {
 
         try {
             logger.info(`Sending message at the index: ${randomIndex}`);
-            logger.info(`Sending message: "${message}" to PHONE_NUMBER_2.`);
+            logger.info(`Sending message: "${message}" to ${maskedPhoneNumber}.`);
 
             const response = await client.messages.create({
                 body: message,
                 from: process.env.TWILIO_PHONE_NUMBER,
                 to: phoneNumber
             });
-            logger.info(`SMS sent successfully to PHONE_NUMBER_2: ${response.sid}`);
+            logger.info(`SMS sent successfully to ${maskedPhoneNumber}: ${response.sid}`);
             return {statusCode: 200, body: JSON.stringify({success: true, message: "SMS sent successfully."})};
         } catch (error) {
-            logger.info(`Attempt ${attempt + 1}: Failed to send SMS to PHONE_NUMBER_2: ${error.message}`);
+            logger.info(`Attempt ${attempt + 1}: Failed to send SMS to ${maskedPhoneNumber}: ${error.message}`);
 
             if (error.response) {
                 logger.error(`SMS send error: Server responded with status ${error.response.status}`);
